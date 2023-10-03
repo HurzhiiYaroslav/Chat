cd8a9f72a587c84592bd6444b0e4f4f692d26c73
HurzhiiYaroslav <136824560+HurzhiiYaroslav@users.noreply.github.com>
2023-10-03 17:52:36 +0300

    Delete unnecessary directory

--- reactap/reactapp/src/Pages/NotFound/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import "./NotFound.scss"
-
-const NotFound = () => {
-    return <div className="NotFound">404</div>;
-};
-export default NotFound;
